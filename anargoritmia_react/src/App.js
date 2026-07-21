d1c74584149ea7da30d8955b30f3718c9a588421
PDE-H14 <escamilla.huerta.porfirio@gmail.com>
2026-07-20 22:55:01 -0600

    Commit inicial

---
+++ anargoritmia_react/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Navigation } from "./routes/index";
+
+import { ClientLayout } from "./layouts";
+
+export default function App() {
+  return (
+    <ClientLayout>
+      <h1>Hola mundo</h1>
+      <button class="ui secondary button">Okay</button>
+      <button class="ui button">Cancel</button>
+      <Navigation />
+    </ClientLayout>
+  );
+}
---
+++ anargoritmia_react/src/routes/Navigation.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+export function Navigation() {
+  return <div>Navigation</div>;
+}
