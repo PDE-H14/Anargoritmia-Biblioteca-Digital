ca5440c6aabe6eeba23c2b20c763b7ccc5ebc488
PDE-H14 <escamilla.huerta.porfirio@gmail.com>
2026-07-23 18:01:50 -0600

    Configuración uso para el uso de JWT

--- anargoritmia_django/anargoritmia/anargoritmia/settings.py
+++ anargoritmia_django/anargoritmia/anargoritmia/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'drf_yasg',
-    "rest_framework",
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'anargoritmia',
 ]
 
 MIDDLEWARE = [
@@ -100,6 +102,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
