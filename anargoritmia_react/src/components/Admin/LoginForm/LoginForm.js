42c90306c868d1296f03105c360957d5b00b31f7
PDE-H14 <escamilla.huerta.porfirio@gmail.com>
2026-07-25 20:38:10 -0600

    endpoint login del admin

--- anargoritmia_react/src/components/Admin/LoginForm/LoginForm.js
+++ anargoritmia_react/src/components/Admin/LoginForm/LoginForm.js
@@ -1,15 +1,50 @@
 import React from "react";
 import { Button, Form } from "semantic-ui-react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import "./LoginForm.scss";
 
-export function LoginForm(props) {
+export function LoginForm() {
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: (formValue) => {
+      console.log("submit");
+      console.log(formValue);
+    },
+  });
   return (
     <div className="login-form">
-      <Form className="login-form-admin">
-        <Form.Input name="email" placeholder="Correo electrónico" />
-        <Form.Input name="password" type="password" placeholder="Contraseña" />
+      <Form className="login-form-admin" onSubmit={formik.handleSubmit}>
+        <Form.Input
+          name="email"
+          placeholder="Correo electrónico"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          error={formik.errors.email}
+        />
+        <Form.Input
+          name="password"
+          type="password"
+          placeholder="Contraseña"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          error={formik.errors.password}
+        />
         <Button type="submit" content="Iniciar sesión" primary fluid />
       </Form>
     </div>
   );
+}
+function initialValues() {
+  return {
+    email: "",
+    password: "",
+  };
+}
+function validationSchema() {
+  return {
+    email: Yup.string().email("Correo inválido").required("Requerido"),
+    password: Yup.string().required("Requerido"),
+  };
 }
--- inicio.md
+++ inicio.md
@@ -1,8 +1,8 @@
 inicio django
 
-C:\Users\pescamillah1800\Desktop\Anargoritmia\anargoritmia_django> .\envs\anargoritmia\Scripts\activate
-C:\Users\pescamillah1800\Desktop\Anargoritmia\anargoritmia_django\anargoritmia> python manage.py runserver
+Anargoritmia\anargoritmia_django> .\envs\anargoritmia\Scripts\activate
+\Anargoritmia\anargoritmia_django\anargoritmia> python manage.py runserver
 
 inicio React
 
-C:\Users\pescamillah1800\Desktop\Anargoritmia\anargoritmia_react> yarn start
+Anargoritmia\anargoritmia_react> yarn start
