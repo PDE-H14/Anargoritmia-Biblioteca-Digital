af03637bbdb3af74e22745f14f1b4e92ceab05ae
PDE-H14 <escamilla.huerta.porfirio@gmail.com>
2026-07-28 23:06:23 -0600

    Cambios esteticos al login

--- anargoritmia_react/src/pages/Admin/LoginAdmin/LoginAdmin.js
+++ anargoritmia_react/src/pages/Admin/LoginAdmin/LoginAdmin.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./LoginAdmin.scss";
 import { LoginForm } from "../../../components/Admin";
+import logo from "../../../assets/images/logoAnargoritmia.png";
 
 export function LoginAdmin() {
   return (
     <div className="login-admin">
       <div className="login-admin__content">
+        <img src={logo} className="login-admin__logo" />
         <h1>INICIAR SESIÓN</h1>
         <LoginForm />
       </div>
